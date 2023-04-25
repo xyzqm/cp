f7526fa11dcb0e0fcda0454f33ea8561eb9ec21e
devolution <danielfangzhu@gmail.com>
2023-04-25 10:22:27 -0700

    Update valiants_new_map.cpp

--- Codeforces/valiants_new_map.cpp
+++ Codeforces/valiants_new_map.cpp
@@ -16,24 +16,15 @@ int main() {
         for (int dif = 1e6; dif > 0; dif /= 2) {
             while (l + dif <= min(n, m)) {
                 int nl = l + dif;
-                // cout << nl << endl;
                 bool pos = false;
                 for (int i = 1; i <= n; i++) {
                     for (int j = 1; j <= m; j++) {
                         pref[i][j] = pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + (a[i][j] >= nl);
                         if (j >= nl && i >= nl) {
                             pos = pos || (pref[i][j] - pref[i][j - nl] - pref[i - nl][j] + pref[i - nl][j - nl] == nl * nl);
-                            // if (pos) break;
                         }
                     }
-                    // if (pos) break;
                 }
-                // for (int i = 1; i <= n; i++) {
-                //     for (int j = 1; j <= m; j++) {
-                //         cout << pref[i][j] << " ";
-                //     }
-                //     cout << endl;
-                // }
                 if (pos) l += dif;
                 else break;
             }
