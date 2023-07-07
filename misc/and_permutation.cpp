6aa2b17d5fe36ea7b3a217880d8c5461269889ee
devo1ution <danielfangzhu@gmail.com>
2023-07-07 14:33:03 -0700

    solve and permutation

---
+++ misc/and_permutation.cpp
@@ -0,0 +1,26 @@
+// https://open.kattis.com/problems/andpermutation
+#include <iostream>
+#include <map>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int N = 1e6;
+ll a[N], b[N];
+int main() {
+    int n; cin >> n;
+    map<ll, int> ind;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        ind[b[i] = a[i]] = i;
+    }
+    for (int B = 0; B < 60; B++) {
+        for (int i = 0; i < n; i++) {
+            if (a[i] & (1LL << B)) {
+                int j = ind[b[i] - (1LL << B)];
+                swap(b[i], b[j]);
+                ind[b[i]] = i, ind[b[j]] = j;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) cout << b[i] << endl;
+}
