2114a542d83627eda0bfa96ce78c1c9456644a22
devo1ution <danielfangzhu@gmail.com>
2023-07-16 10:17:00 -0700

    run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+start=$SECONDS
+file=$(find . -type f -name "*.cpp" | xargs ls -t | head -1)
+g++ -std=c++17 $file
+echo "compiled $file in $((SECONDS - start)) seconds"
+start=$SECONDS
+./a.out
+echo "finished in $((SECONDS - start)) seconds"
