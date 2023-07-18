0a19c7f62d9f7c1b0333c7f811ee8d6b56ba671b
devo1ution <danielfangzhu@gmail.com>
2023-07-18 07:56:56 -0700

    solve legacy

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 start=$SECONDS
 file=$(find . -type f -name "*.cpp" | xargs ls -t | head -1)
-g++ -std=c++17 $file
+g++ -std=c++17 -Wall $file
 echo "compiled $file in $((SECONDS - start)) seconds"
 start=$SECONDS
 ./a.out
