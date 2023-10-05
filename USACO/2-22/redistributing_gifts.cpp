c1efc1b806dfc3314f168c9899beb2a16dbdea8b
devo1ution <danielfangzhu@gmail.com>
2023-10-04 23:23:47 -0700

    solve redistributing gifts yay

--- USACO/2-22/redistributing_gifts.cpp
+++ USACO/2-22/redistributing_gifts.cpp
@@ -11,34 +11,29 @@ using T = tuple<int, int, int>;
 const int N = 18;
 int n, q;
 int p[N][N];
-int w[1 << N], dp[1 << N], t[1 << N][N];
+int dp[1 << N][N], ans[1 << N];
 signed main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             int x; cin >> x;
-            --x;
-            p[i][x] = j;
+            p[i][--x] = j;
         }
     }
-    for (int i = 0; i < n; i++) {
-        t[1 << i][i] = 1;
-        for (int _ = 0; _ < (1 << (n - i)); _++) {
-            int j = _ << i;
-            for (int k = i; k < n; k++) {
-                if (!t[j][k]) continue;
-                for (int h = i; h < n; h++) if (p[h][k] <= p[h][h]) {
-                    if (h == i) w[j] += t[j][k];
-                    else if (!(j >> h & 1)) t[j | (1 << h)][h] += t[j][k];
-                }
+    for (int i = 0; i < n; i++) dp[1 << i][i] = 1;
+    for (int i = 0; i < 1 << n; i++) {
+        for (int j = 0; j < n; j++) if (dp[i][j]) {
+            for (int k = 0; k < n; k++) if (p[k][j] <= p[k][k]) {
+                int I = __builtin_clzll(i), K = __builtin_clzll(1 << k);
+                if (I == K) {
+                    ans[i] += dp[i][j];
+                    for (int h = 64 - I; h < n; h++) dp[i | 1 << h][h] += dp[i][j];
+                } else if (K > I && !(i >> k & 1)) dp[i | 1 << k][k] += dp[i][j]; 
             }
         }
     }
-    dp[0] = 1;
-    for (int i = 1; i < 1 << n; i++) {
-        for (int j = i; j; j = (j - 1) & i) if ((i & -i) == (j & -j)) dp[i] += w[j] * dp[i - j];
-    }
+    ans[0] = 1;
     cin >> q;
     while (q--) {
         int h = 0, g = 0;
@@ -47,7 +42,7 @@ signed main() {
             if (c == 'H') h |= 1 << i;
             else g |= 1 << i;
         }
-        cout << dp[h] * dp[g] << '\n';
+        cout << ans[h] * ans[g] << '\n';
     }
     return 0;
 }
