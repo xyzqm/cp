b79d310076a7a29ed8d95f410a65abc44bccc5b7
devo1ution <danielfangzhu@gmail.com>
2023-12-16 23:34:54 -0800

    solve gold dance mooves!

--- USACO/1-21/dance_mooves.cpp
+++ USACO/1-21/dance_mooves.cpp
@@ -3,9 +3,11 @@
 #include <tuple>
 #include <algorithm>
 #include <set>
+#include <functional>
 using namespace std;
 #define int long long
 const int inf = 1e18;
+using P = pair<int, int>;
 template<typename... Args>
 void print(Args... args)
 {
@@ -13,71 +15,72 @@ void print(Args... args)
     cout << endl;
 }
 const int N = 1e5 + 1, K = 2e5 + 1;
-int n, k, m, R, x[N], X[N], Y[N], in[N], a[N], p[N], S[N], v[N], c[N], r[N], b[K];
-set<int> s[N];
+int n, k, m, unq, jumps, I, x[N], X[N], Y[N], a[K], v[N], c[N], r[N], prv[N], cyc[N], b[K];
 vector<int> g[N];
-int P(int x) { return p[x] = p[x] == x ? x : P(p[x]); }
-int U(int x, int y) {
-    x = P(x), y = P(y);
-    if (x == y) return 0;
-    if (s[x] < s[y]) swap(x, y);
-    p[y] = x, S[x] += S[y];
-}
+vector<P> s[N];
 int floyd(int x) {
     int t = x, h = X[x];
     while (t != h) t = X[t], h = X[X[h]];
     return t;
 }
-void dfs(int x) {
-    v[x] = true;
-    for (int i : s[x]) if (!c[i]++) R++;
-    r[x] = R;
-    for (int i : g[x]) if (!v[i]) dfs(i);
-    for (int i : s[x]) if (!--c[i]) R--;
-}
 signed main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> n >> k >> m;
-    for (int i = 0; i < k; i++) cin >> a[i] >> b[i];
-    for (int i = 1; i <= n; i++) s[i].insert(i), S[i] = 1, p[i] = x[i] = Y[i] = i;
-    for (int i = 0; i < k; i++) swap(Y[a[i]], Y[b[i]]);
-    for (int i = 1; i <= n; i++) X[Y[i]] = i;
-    // for (int i = 1; i <= n; i++) cout << X[i] << " ";
-    // cout << endl;
-    for (int i = 1; i <= n; i++) g[X[i]].push_back(i);
-    if (n <= 100 && k <= 200 && m < n * k) {
-        for (int _ = 0; _ < n; _++) {
-            for (int __ = 0; __ < k; __++) if (m) {
-                swap(x[a[__]], x[b[__]]);
-                s[x[a[__]]].insert(a[__]);
-                s[x[b[__]]].insert(b[__]);
-                m--;
-            }
-        }
-        for (int i = 1; i <= n; i++) cout << s[i].size() << endl;
-    } else {
-        for (int i = 0; i < k; i++) {
-            swap(x[a[i]], x[b[i]]);
-            s[x[a[i]]].insert(a[i]);
-            s[x[b[i]]].insert(b[i]);
-        }
-        for (int i = 1; i <= n; i++) if (!v[i]) {
-            int j = floyd(i);
-            int cur = j;
-            // print(i, j);
-            for (int _ = 1; _ <= n; _++) c[_] = 0;
-            R = 0;
-            do {
-                cur = X[cur], v[cur] = true;
-                for (int _ : s[cur]) if (!c[_]++) R++;
-            } while (cur != j);
-            do {
-                cur = X[cur];
-                r[cur] = R;
-                for (int _ : g[cur]) if (!v[_]) dfs(_);
-            } while (cur != j);
-        }
-        for (int i = 1; i <= n; i++) cout << r[i] << endl;
+    for (int i = 1; i <= k; i++) cin >> a[i] >> b[i];
+    for (int i = 1; i <= n; i++) {
+        s[i].push_back({0, i});
+        x[i] = Y[i] = i;
     }
+    for (int i = 1; i <= k; i++) swap(Y[a[i]], Y[b[i]]);
+    for (int i = 1; i <= n; i++) g[X[Y[i]] = i].push_back(Y[i]);
+    for (int i = 1; i <= k; i++) {
+        swap(x[a[i]], x[b[i]]);
+        s[x[a[i]]].push_back({i, a[i]});
+        s[x[b[i]]].push_back({i, b[i]});
+    }
+    for (int i = 1; i <= n; i++) if (!v[i]) {
+        int max_jumps = m / k, max_steps = m % k;
+        int root = floyd(i);
+        int cur = root;
+        for (int j = 1; j <= n; j++) c[j] = 0;
+        unq = 0, jumps = 0, I = 0;
+        if (max_jumps) do {
+            for (auto [_, j] : s[cur]) if (!c[j]++) unq++;
+            ++jumps;
+            prv[X[cur]] = cur, cur = X[cur];
+        } while (jumps < max_jumps && cur != root);
+        // jumps now equals min(cycle length, max_jumps)
+        // if we haven't already jumped the cycle, start stepping
+        auto step = [&](int mx) { 
+            while (I < s[cur].size() && s[cur][I].first <= mx) {
+                if (!c[s[cur][I++].second]++) unq++;
+            }
+        };
+        if (jumps == max_jumps) step(max_steps);
+        v[root] = true, r[root] = unq;
+        function<void(int)> dfs = [&](int x) {
+            v[x] = true;
+            if (--max_jumps < jumps) {
+                while (I && I--) if (!--c[s[cur][I].second]) unq--;
+                cur = prv[cur];
+                I = s[cur].size();
+                while (I-- && s[cur][I].first > max_steps) if (!--c[s[cur][I].second]) unq--;
+                ++I;
+            }
+            for (auto [_, j] : s[x]) if (!c[j]++) unq++;
+            r[x] = unq;
+            for (int y : g[x]) if (!v[y]) {
+                dfs(prv[x] = y);
+            }
+            for (auto [_, j] : s[x]) if (!--c[j]) unq--;
+            if (++max_jumps <= jumps) {
+                step(inf);
+                cur = X[cur];
+                step(max_steps);
+            }
+        };
+        for (int j : g[root]) if (!v[j]) dfs(prv[root] = j);
+    }
+    for (int i = 1; i <= n; i++) cout << r[i] << endl;
     return 0;
 }
