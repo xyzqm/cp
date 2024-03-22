0c69775045add8fd8d1d38d02ca97340ee61b0d0
Daniel Zhu <danielfangzhu@gmail.com>
2024-03-21 20:57:56 -0700

    solve falling portals

--- USACO/1-20/falling_portals.cc
+++ USACO/1-20/falling_portals.cc
@@ -26,13 +26,9 @@ struct F {
   bool operator<(const F& o) const { return n * o.d < d * o.n; }
 };
 
-int n, a[N], q[N];
-F r[N];
-
-F m(int i, int j) { return F{a[j] - a[i], j - i}.reduce(); }
-
 struct Q {
-  int i, f, x, y;
+  int i, f, x;
+  ll y;
 
   bool operator<(const Q& o) const {
     if (y != o.y) return y < o.y;
@@ -43,8 +39,37 @@ struct Q {
 struct Convex {
   // store points in clockwise order
   vector<int> hull;
+  int n;
   int l = inf; // leftmost point
+  ll a[N]; // y-coordinates
+  vector<Q> q; // queries
+  F r[N]; // answers for each query
 
+  // sneaky little override >:)
+  void upd(int i, ll y) {
+    a[i] = y;
+    q.push_back({-1, i, i, y});
+  }
+
+  // register path from (i, a[i]) to (x, a[x]) in list of queries
+  void upd(int i, int x) {
+    assert(a[x] >= a[i]);
+    q.push_back({x, i, min(i, x), a[i]});
+  }
+
+  // get accepted :)
+  void ac() {
+    for (int i = 0; i < n; i++) r[i] = {1, 0};
+    sort(q.begin(), q.end());
+    for (Q ev : q) {
+      if (ev.i < 0) add(ev.f);
+      else r[ev.f] = min(r[ev.f], query(ev));
+    }
+  }
+
+  F m(int i, int j) { return F{a[j] - a[i], j - i}.reduce(); }
+
+  // whether i -> j -> k constitutes a left turn
   bool left(int i, int j, int k) {
     if (i < 0) return false;
     assert(i >= 0 && j >= 0 && k >= 0);
@@ -52,86 +77,56 @@ struct Convex {
     return (a[k] - a[i]) * (j - i) - (a[j] - a[i]) * (k - i) >= 0;
   }
 
+  // adds point (i, a[i]) into the hull
   void add(int i) {
-    // cout << "adding " << i << endl;
     while (left(hull.size() - 2, hull.size() - 1, i)) hull.pop_back();
     if (!hull.size() || a[i] > a[hull.back()]) hull.push_back(i);
-    if (l >= hull.size() || i < hull[l]) l = i;
-    // cout << "added" << endl;
+    if (l >= hull.size() || i < hull[l]) l = hull.size() - 1;
   }
 
   F query(Q q) {
-    // cout << "querying " << q.i << " " << q.x << " " << q.y << endl;
-    // cout << "l: " << l << endl;
-    // for (int x : hull) cout << x << " ";
-    // cout << endl;
     assert(q.i >= 0);
     int x = hull.size();
     for (int dif = hull.size(); dif > 0; dif /= 2) {
       while (x - dif >= l && hull[x - dif] > q.x) x -= dif;
     }
-    int y = 0;
+    int y = -1;
     for (int dif = hull.size(); dif > 0; dif /= 2) {
       while (y + dif <= l && hull[y + dif] > q.x) y += dif;
     }
     ++y;
     for (int dif = hull.size(); dif > 0; dif /= 2) {
-      while (y + dif < x && hull[y + dif] <= q.y && !left(y + dif - 1, y + dif, q.i)) y += dif;
+      while (y + dif < x && !left(y + dif - 1, y + dif, q.i)) y += dif;
     }
-    // cout << y << " " << m(hull[y], q.i).str() << endl;
-    if (y >= 0 && y < hull.size() && a[hull[y]] <= q.y && hull[y] <= q.x && !left(y - 1, y, q.i)) return m(hull[y], q.i);
+    if (
+      y >= 0 && y < hull.size()
+      && a[hull[y]] <= q.y && hull[y] <= q.x
+      && !left(y - 1, y, q.i)
+    ) {
+      return m(hull[y], q.i);
+    }
     else return {1, 0};
   }
-} hull;
+} hull, rhull;
 
 int main() {
   freopen("falling.in", "r", stdin);
   freopen("falling.out", "w", stdout);
-  cin >> n;
-  vector<Q> qs;
+  int n; cin >> n;
+  hull.n = rhull.n = n;
   for (int i = 0; i < n; i++) {
-    cin >> a[i];
-    qs.push_back({-1, i, i, a[i]});
-  }
-  for (int i = 0; i < n; i++) {
-    cin >> q[i];
-    --q[i], r[i] = {1, 0};
+    ll y; cin >> y;
+    hull.upd(i, y);
+    rhull.upd(n - i - 1, -y);
   }
   for (int i = 0; i < n; i++) {
-    // cout << i << " " << q[i] << " " << a[i] << " " << a[q[i]] << endl;
-    // case 1: up-right
-    if (i < q[i] && a[i] <= a[q[i]]) {
-      // for (int j = 0; j <= i; j++) if (a[j] <= a[i]) {
-      //   r[i] = min(r[i], m(j, q[i]));
-      // }
-      qs.push_back({q[i], i, i, a[i]});
-    }
-    // case 2: down-left
-    else if (q[i] < i && a[q[i]] <= a[i]) {
-      r[i] = m(q[i], i);
-      for (int j = i; ++j < n; ) if (a[j] > a[i]) {
-        r[i] = min(r[i], m(q[i], j));
-      }
-    }
-    // case 3: down-right
-    else if (i < q[i] && a[i] > a[q[i]]) {
-      for (int j = q[i]; ++j < n; ) if (a[j] > a[i]) {
-        r[i] = min(r[i], m(q[i], j));
-      }
-    }
-    // case 4: up-left
-    else if (q[i] < i && a[q[i]] > a[i]) {
-      // for (int j = 0; j < q[i]; j++) if (a[j] < a[i]) {
-      //   r[i] = min(r[i], m(j, q[i]));
-      // }
-      qs.push_back({q[i], i, q[i], a[i]});
-    }
-    else assert(false);
+    int x; cin >> x; --x;
+    if (hull.a[i] <= hull.a[x]) hull.upd(i, x);
+    else rhull.upd(n - 1 - i, n - 1 - x);
   }
-  sort(qs.begin(), qs.end());
-  for (Q ev : qs) {
-    if (ev.i < 0) hull.add(ev.f);
-    else r[ev.f] = min(r[ev.f], hull.query(ev));
+  hull.ac(); rhull.ac();
+  for (int i = 0; i < n; i++) {
+    F r = min(hull.r[i], rhull.r[n - 1 - i]);
+    cout << (r.d ? r.str() : "-1") << "\n";
   }
-  for (int i = 0; i < n; i++) cout << (r[i].d ? r[i].str() : "-1") << "\n";
 }
