f8ac299aaceaa24bb7ebd52e0c21932600a93c39
Daniel Zhu <danielfangzhu@gmail.com>
2024-05-10 22:56:39 -0700

    resolve cookie distribution

--- Atcoder/cookie_distribution.cc
+++ Atcoder/cookie_distribution.cc
@@ -1,38 +1,40 @@
+// https://atcoder.jp/contests/dwacon6th-prelims/tasks/dwacon6th_prelims_c
 #include <iostream>
-#include <algorithm>
+#define int long long
 using namespace std;
-using ll = long long;
+
+const int inf = 1e18;
+
 const int N = 1e3 + 1;
+const int K = 21;
+int n, k, a[K], dp[K][N];
+
 const int M = 1e9 + 7;
-ll dp[21][N], fact[N], inv[N];
-ll pw(ll a, int b) {
-    ll res = 1;
-    while (b) {
-        if (b % 2) res = res * a % M, --b;
-        else b /= 2, a = a * a % M;
-    }
-    return res;
+int E(int x, int y) {
+  int r = 1;
+  for (y <<= 1; y >>= 1; x = x * x % M) if (y & 1) r = r * x % M;
+  return r;
 }
-ll c(int n, int k) { return fact[n] * inv[k] % M * inv[n - k] % M; }
-int main() {
-    fact[0] = inv[0] = 1;
-    for (int i = 1; i < N; i++) {
-        fact[i] = (fact[i - 1] * i) % M;
-        inv[i] = pw(fact[i], M - 2);
+int inv(int x) { return E(x, M - 2); }
+int ad(int& x, int y) { return x = (x + y) % M; }
+
+int F[N], F_i[N];
+void pre(int n) { F[0] = F_i[0] = 1; for (int i = 1; i < n; i++) F_i[i] = inv(F[i] = F[i - 1] * i % M); }
+int C(int n, int k) { return F[n] * F_i[n - k] % M * F_i[k] % M; }
+
+signed main() {
+  pre(N);
+  cin.tie(0)->sync_with_stdio(0); 
+  cin >> n >> k;
+  for (int i = 0; i < k; i++) cin >> a[i];
+  dp[0][0] = 1;
+  for (int i = 0; i < k; i++) {
+    for (int j = 0; j <= n; j++) if (dp[i][j]) {
+      for (int h = 0; h <= min(n - j, a[i]); h++) {
+        ad(dp[i + 1][j + h], C(n - j, h) * C(n - h, a[i] - h) % M * dp[i][j] % M); 
+      }
     }
-    int n, K;
-    cin >> n >> K;
-    dp[0][0] = 1;
-    int a;
-    for (int i = 1; i <= K; i++) {
-        cin >> a;
-        for (int j = 0; j <= n; j++) { // number of final red cookies
-            for (int k = 0; k <= min(a, j); k++) { // number of new red cookies
-                ll red = dp[i - 1][j - k] * c(n - j + k, k) % M; // choose the k new red cookies
-                ll rest = c(n - k, a - k);
-                dp[i][j] = (dp[i][j] + red * rest) % M;
-            }
-        }
-    }
-    cout << dp[K][n] << endl;
+  }
+  cout << dp[k][n] << endl;
+
 }
