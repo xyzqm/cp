2c41c7049128be654ab7c04eae5a3818d45ca72e
Daniel Zhu <danielfangzhu@gmail.com>
2024-06-21 16:01:28 -0700

    add poly inverse

--- .impls/fft.cc
+++ .impls/fft.cc
@@ -1,9 +1,9 @@
 #include <array>
 #include <cassert>
 #include <iostream>
+#include <utility>
 using namespace std;
-
-const int N = 1 << 20;
+const int N = 1 << 19;
 
 using ll = long long;
 struct mint {
@@ -21,23 +21,25 @@ struct mint {
 istream& operator>>(istream& s, mint& v) { s >> v.v; return s; }
 ostream& operator<<(ostream& s, const mint& v) { s << v.v; return s; }
 
+const mint i2 = mint(2).inv();
+
 template <int N> struct F {
-  array<mint, N> a;
   static inline const mint w = mint(3).E((mint::M - 1) / N);
   static inline const mint iw = w.inv();
-  static inline const mint i2 = mint(2).inv();
   static inline mint p[N]{}, ip[N]{};
-  F<N>() { 
-    if (p[0].v) return;  
+  array<mint, N> a{};
+  F<N>() {
+    if (p[0].v) return;
     p[0] = 1; for (int i = 1; i < N; i++) p[i] = p[i - 1] * w; 
     ip[0] = 1; for (int i = 1; i < N; i++) ip[i] = ip[i - 1] * iw;
   }
-  // in-place fft/ifft
+  template<int K>
+  F<K> slice() const { F<K> f; for (int i = 0; i < min(K, N); i++) f[i] = a[i]; return f; }
   F<N>& fft(bool inv = false) {
     for (int _ = 1; _ < N; _ <<= 1) {
       int s = inv ? _ : N / 2 / _; // stride
       for (int i = 0; i < N; i++) if (!(i & s)) {
-        mint W = (inv ? ip : p)[N / 2 / s * (i & (s - 1))]; 
+        mint W = (inv ? ip : p)[N / 2 / s * (i & (s - 1))];
         mint x = a[i], y = a[i + s];
         if (inv) a[i] = (x + y * W) * i2, a[i + s] = (x - y * W) * i2;
         else a[i] = x + y, a[i + s] = (x - y) * W;
@@ -45,25 +47,49 @@ template <int N> struct F {
     } 
     return *this;
   }
+  mint& operator[](int i) { return a[i]; }
   // pointwise multiplication
-  void operator*= (const F<N>& f) { for (ll i = 0; i < N; i++) a[i] = a[i] * f.a[i]; }
+  void operator*= (F<N>& f) { for (int i = 0; i < N; i++) a[i] = a[i] * f[i]; }
+  // pointwise multiplication by scalar
+  F<N> operator* (const mint &v) const { F<N> f = *this; for (int i = 0; i < N; i++) f[i] = f[i] * v; return f; }
+  // addition by scalar
+  F<N> operator+ (const mint& v) const { F<N> f = *this; f[0] = f[0] + v; return f; }
   // convolution
-  F<N> operator* (const F<N>& o) {
-    F<N> A = *this, B = o;
+  F<N << 1> operator* (const F<N> &&o) const {
+    F<N << 1> A = slice<N << 1>(), B = o.template slice<N << 1>();
     A.fft() *= B.fft();
     return A.fft(true);
+  }
+  F<N> *v;
+  // multiplicative inverse (mod x^N)
+  template <int K = 1>
+  inline constexpr void inv_internal(F<K> &&f) {
+    if constexpr (K == N) *v = f;
+    else {
+      if constexpr (K == 1) f[0] = a[0].inv();
+      F<K << 1> h;
+      {
+        auto F = f.template slice<K << 1>();
+        auto g = F * (F * slice<K << 1>() * -1 + 2).template slice<K << 1>();
+        h = g.template slice<K << 1>();
+      }
+      inv_internal<K << 1>(std::move(h));
+    }
+  }
+  F<N> inv() {
+    v = new F<N>(); F<1> f; inv_internal(std::move(f));
+    return *v;
   }
 };
 
 
 signed main() {
   cin.tie(0)->sync_with_stdio(0); 
-  F<N> f, g;
-  int n, m; cin >> n >> m;
-  for (int i = 0; i < n; i++) cin >> f.a[i];
-  for (int i = 0; i < m; i++) cin >> g.a[i];
-  F<N> h = f * g;
-  for (int i = 0; i < n + m - 1; i++) cout << h.a[i] << " ";
+  F<N> f;
+  int n; cin >> n;
+  for (int i = 0; i < n; i++) cin >> f[i];
+  F<N> h = f.inv();
+  for (int i = 0; i < n; i++) cout << h.a[i] << " ";
   cout << endl;
 }
 
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 start=$SECONDS
 file=$(find -E . -regex '.*\.(cc|cpp)' | xargs ls -t | head -1)
 echo "compiling $file"
-g++ -std=c++17 -Wall $file -g -fsanitize=address -fsanitize=undefined || exit 1
+g++ -std=c++17 -O3 -Wall $file -g -fsanitize=address -fsanitize=undefined || exit 1
 echo "compiled $file in $((SECONDS - start)) seconds"
 start=$SECONDS
 ./a.out <a.in
