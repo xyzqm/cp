6c53d76ef245c4ca8418b8d6bad0f7c0d5b1249f
Daniel Zhu <danielfangzhu@gmail.com>
2024-06-21 10:22:35 -0700

    better fft implementation

--- .impls/fft.cc
+++ .impls/fft.cc
@@ -1,49 +1,69 @@
-// https://loj.ac/p/108
+#include <array>
+#include <cassert>
 #include <iostream>
-#include <complex>
-#include <string>
-#include <vector>
 using namespace std;
-using cd = complex<double>;
 
-const double PI = acos(-1);
+const int N = 1 << 20;
 
-// evaluates the polynomial sum(a[i] * x^i) at w^[0...n - 1]
-template <typename T>
-vector<T> fft(vector<T> a, T w) {
-  if (a.size() == 1) return a;
-  int n = a.size();
-  vector<T> e(n / 2), o(n / 2);
-  for (int i = 0; i < a.size(); i++) {
-    (i % 2 ? o : e)[i / 2] = a[i];
+using ll = long long;
+struct mint {
+  const static ll M = 998244353;
+  ll v = 0;
+  mint() {}
+  mint(ll v) { this->v = (v % M + M) % M; }
+  mint operator+(const mint &o) const { return v + o.v; }
+  mint operator*(const mint &o) const { return v * o.v; }
+  mint operator-(const mint &o) const { return v - o.v; }
+  mint E(ll y) const { mint r = 1, x = v; for (y <<= 1; y >>= 1; x = x * x) if (y & 1) r = r * x; return r; }
+  mint inv() const { return E(M - 2); }
+  mint operator/(mint o) { return *this * o.inv(); }
+};
+istream& operator>>(istream& s, mint& v) { s >> v.v; return s; }
+ostream& operator<<(ostream& s, const mint& v) { s << v.v; return s; }
+
+template <int N> struct F {
+  array<mint, N> a;
+  static inline const mint w = mint(3).E((mint::M - 1) / N);
+  static inline const mint iw = w.inv();
+  static inline const mint i2 = mint(2).inv();
+  static inline mint p[N]{}, ip[N]{};
+  F<N>() { 
+    if (p[0].v) return;  
+    p[0] = 1; for (int i = 1; i < N; i++) p[i] = p[i - 1] * w; 
+    ip[0] = 1; for (int i = 1; i < N; i++) ip[i] = ip[i - 1] * iw;
   }
-  vector<T> E = fft(e, w * w);
-  vector<T> O = fft(o, w * w);
-  vector<T> r(n);
-  T c = 1;
-  for (int i = 0; i < r.size(); i++) {
-    r[i] = E[i % (n / 2)] + c * O[i % (n / 2)];
-    c *= w;
+  // in-place fft/ifft
+  F<N>& fft(bool inv = false) {
+    for (int _ = 1; _ < N; _ <<= 1) {
+      int s = inv ? _ : N / 2 / _; // stride
+      for (int i = 0; i < N; i++) if (!(i & s)) {
+        mint W = (inv ? ip : p)[N / 2 / s * (i & (s - 1))]; 
+        mint x = a[i], y = a[i + s];
+        if (inv) a[i] = (x + y * W) * i2, a[i + s] = (x - y * W) * i2;
+        else a[i] = x + y, a[i + s] = (x - y) * W;
+      }
+    } 
+    return *this;
   }
-  return r;
-}
+  // pointwise multiplication
+  void operator*= (const F<N>& f) { for (ll i = 0; i < N; i++) a[i] = a[i] * f.a[i]; }
+  // convolution
+  F<N> operator* (const F<N>& o) {
+    F<N> A = *this, B = o;
+    A.fft() *= B.fft();
+    return A.fft(true);
+  }
+};
 
-int main() {
+
+signed main() {
+  cin.tie(0)->sync_with_stdio(0); 
+  F<N> f, g;
   int n, m; cin >> n >> m;
-  ++n, ++m;
-  vector<cd> a(n), b(m);
-  for (cd& x : a) cin >> x;
-  for (cd& x : b) cin >> x;
-  int k = 1 << (32 - __builtin_clz(n + m));
-  a.resize(k), b.resize(k);
-  cd w(cos(2 * PI / k), sin(2 * PI / k));
-  vector<cd> A = fft(a, conj(w));
-  vector<cd> B = fft(b, conj(w));
-  vector<cd> C(A.size());
-  for (int i = 0; i < C.size(); i++) C[i] = A[i] * B[i];
-  vector<cd> c = fft(C, w);
-  for (int i = 0; i < n + m - 1; i++) {
-    cout << int(round(real(c[i]) / k)) << " ";
-  }
+  for (int i = 0; i < n; i++) cin >> f.a[i];
+  for (int i = 0; i < m; i++) cin >> g.a[i];
+  F<N> h = f * g;
+  for (int i = 0; i < n + m - 1; i++) cout << h.a[i] << " ";
   cout << endl;
 }
+
