41732e8abc70150abe634b361ae4b105978440ea
Daniel Zhu <danielfangzhu@gmail.com>
2024-07-14 09:49:52 -0700

    solve jump

--- misc/noi_jump.cc
+++ misc/noi_jump.cc
@@ -1,6 +1,7 @@
-// https://judge.yosupo.jp/problem/point_add_rectangle_sum
+// https://loj.ac/p/3159
 #include <algorithm>
 #include <cassert>
+#include <cstdio>
 #include <initializer_list>
 #include <iostream>
 #include <queue>
@@ -36,10 +37,15 @@ struct T { // rectangle/prism class
 
 const int N = 2e5 + 1;
 
+struct E { int t; T<2> r;
+  bool operator<(const E o) const { return t > o.t; }
+  E add(int x) { return {t + x, r}; }
+};
+
 template <int K>
 struct KD {
   vector<P<K>> p;
-  int V[4 * N]{}, v[4 * N]{}, t[4 * N]{};
+  int D[4 * N]{}, d[4 * N], t[4 * N]{};
   auto cmp(int d) {
     return [=](P<K> u, P<K> v) {
         if (u[d] != v[d]) return u[d] < v[d]; 
@@ -52,71 +58,73 @@ struct KD {
     p.erase(unique(p.begin(), p.end()), p.end());
     queue<P<4>> q; q.push({1, 0, (int)p.size(), 0});
     fill(t, t + 4 * N, -1);
+    fill(d, d + 4 * N, inf);
+    fill(D, D + 4 * N, inf);
     while (q.size()) {
       auto [i, l, r, d] = q.front(); q.pop();
       if (l >= r) continue;
       int m = (l + r) >> 1;
-      t[i] = m;
       nth_element(p.begin() + l, p.begin() + m, p.begin() + r, cmp(d));
+      t[i] = m;
       q.push({i << 1, l, m, (d + 1) % K});
       q.push({i << 1 | 1, m + 1, r, (d + 1) % K});
     }
   }
-  void upd(P<K> x, int w) {
+  int query(P<K> x) {
     int i = 1, d = 0;
     while (p[t[i]] != x) {
-      V[i] += w;
       if (cmp(d)(x, p[t[i]])) i <<= 1;
       else i = i << 1 | 1;
       d = (d + 1) % K;
       assert(t[i] >= 0);
     }
     assert(x == p[t[i]]);
-    V[i] += w;
-    v[i] += w;
+    return i;
   }
-  int query(T<K> R) {
+  vector<int> upd(E e) { // returns a list of updated nodes
     queue<P<3>> q; q.push({1, 0, 0});  // { cur rectangle, current dimension, bitmask of edges outside current rectangle }
-    int z = 0;
+    vector<int> r; // updated nodes
     while (q.size()) {
-      auto [i, d, e] = q.front(); q.pop();
+      auto [i, d, m] = q.front(); q.pop();
       if (t[i] < 0) continue;
       assert(t[i] < p.size());
       P<K> x = p[t[i]];
-      if (__builtin_popcount(e) == 2 * K) { z += V[i]; continue; }
-      if (R.contains(x)) z += v[i];
-      if (R[d][0] <= x[d]) q.push({i << 1, (d + 1) % K, e | (x[d] <= R[d][1]) * (1 << (2 * d + 1))}); // left/bottom rect
-      if (R[d][1] >= x[d]) q.push({i << 1 | 1, (d + 1) % K, e | (x[d] >= R[d][0]) * (1 << (2 * d))}); // right/top rect
+      if (e.t >= D[i]) continue;
+      if (__builtin_popcount(m) == 2 * K) D[i] = e.t;
+      if (e.r.contains(x) && e.t < this->d[i]) {
+        this->d[i] = e.t;
+        r.push_back(i);
+      }
+      if (e.r[d][0] <= x[d]) q.push({i << 1, (d + 1) % K, m | (x[d] <= e.r[d][1]) * (1 << (2 * d + 1))}); // left/bottom rect
+      if (e.r[d][1] >= x[d]) q.push({i << 1 | 1, (d + 1) % K, m | (x[d] >= e.r[d][0]) * (1 << (2 * d))}); // right/top rect
     }
-    return z;
+    return r;
   }
 };
 
-vector<P<2>> a;
-vector<P<4>> q;
+vector<E> g[N];
+int o[4 * N];
 
 signed main() {
+  freopen("jump.in", "r", stdin);
+  freopen("jump.out", "w", stdout);
   cin.tie(0)->sync_with_stdio(0);
-  int n, Q; cin >> n >> Q;
-  for (int i = 0; i < n; i++) {
-    int x, y, w; cin >> x >> y >> w;
-    a.push_back({x, y});
-    q.push_back({-1, x, y, w});
+  int n, m, w, h; cin >> n >> m >> w >> h;
+  vector<P<2>> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i][0] >> a[i][1];
+  for (int i = 0; i < m; i++) {
+    int p, t, l, r, d, u; cin >> p >> t >> l >> r >> d >> u;
+    g[--p].push_back({t, {{l, r}, {d, u}}});
   }
-  while (Q--) {
-    int t; cin >> t;
-    if (t) {
-      int l, d, r, u; cin >> l >> d >> r >> u;
-      q.push_back({l, d, r, u});
-    } else {
-      int x, y, w; cin >> x >> y >> w;
-      a.push_back({x, y});
-      q.push_back({-1, x, y, w});
+  priority_queue<E> q;
+  for (E e : g[0]) q.push(e);
+  KD<2> t(a);
+  for (int i = 0; i < n; i++) o[t.query(a[i])] = i;
+  while (q.size()) {
+    E e = q.top(); q.pop();
+    for (int x : t.upd(e)) {
+      for (E e_ : g[o[x]]) q.push(e_.add(e.t));
     }
   }
-  KD<2> t(a);
-  for (auto [a, b, c, d] : q) {
-    if (a < 0) t.upd({b, c}, d);
-    else cout << t.query({{{a, c - 1}, {b, d - 1}}}) << endl;
-  }
+  for (int i = 1; i < n; i++) cout << t.d[t.query(a[i])] << endl;
 }
