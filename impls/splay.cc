0d369ab583b0264aaeb6371534a0af1a833d7298
Daniel Zhu <danielfangzhu@gmail.com>
2024-08-06 08:28:29 -0700

    improve splay impl

--- impls/splay.cc
+++ impls/splay.cc
@@ -1,11 +1,9 @@
 #include <cassert>
 #include <functional>
 #include <iostream>
-#include <vector>
 #define int long long
 using namespace std;
 
-struct mint;
 struct mint {
   const static int M = 998244353;
   int v = 0;
@@ -16,12 +14,8 @@ struct mint {
   mint operator*(const mint &o) const { return v * o.v; }
   mint operator-(const mint &o) const { return v - o.v; }
   mint& operator-=(const mint& o) { mint t = v - o.v; v = t.v; return *this; }
-  mint operator^(int y) const { mint r = 1, x = v; for (y <<= 1; y >>= 1; x = x * x) if (y & 1) r = r * x; return r; }
-  mint inv() const { assert(v); return *this ^ M - 2; }
-  mint sym() { return *this ^ (M - 1) / 2; } // legendre symbol: 1 -> qresidue, -1 -> non-residue
   friend istream& operator>>(istream& s, mint& v) { s >> v.v; return s; }
   friend ostream& operator<<(ostream& s, const mint& v) { s << v.v; return s; }
-  mint operator/(mint o) { return *this * o.inv(); }
 };
 
 struct F {
@@ -31,125 +25,129 @@ struct F {
   operator bool() const { return w.v != 1 || b.v != 0; }
 };
 
+const int N = 5e5 + 1;
+struct Node;
 typedef struct Node * T;
 struct Node {
   mint v, s;
   int sz;
-  T l, r;
-  F f;
-  bool rev;
-  static inline vector<T> st = {};
-  Node(int v, T l = nullptr, T r = nullptr) : v(v), s(v), sz(0), l(l), r(r), rev(false) { pull(); }
+  inline static int n = 0;
+  static Node t[2 * N];
+  array<T, 2> c{};
+  T p{};
+  F f{};
+  bool rev = false;
+  Node() {}
+  Node(int v) : v(v), s(v), sz(1), rev(false) {}
+  static T init(int v, T l = nullptr, T r = nullptr) {
+    t[n] = Node(v);
+    t[n].attach(0, l);
+    t[n].attach(1, r);
+    t[n].pull();
+    return &t[n++];
+  }
   static int size(T x) { return x ? x->sz : 0; }
-  static mint sum(T x) { return x ? x->s : 0; } 
+  static mint sum(T x) { return x ? x->s : 0; }
+  int ind() { return this - t; }
   void push() {
     if (rev) {
+      swap(c[0], c[1]);
+      for (int i = 0; i < 2; i++) if (c[i]) c[i]->rev ^= rev;
       rev = false;
-      swap(l, r);
-      if (l) l->rev ^= true;
-      if (r) r->rev ^= true;
     }
     if (f) {
       v = f(v);
       s = f.w * s + f.b * sz;
-      if (l) l->f = f(l->f);
-      if (r) r->f = f(r->f);
+      for (int i = 0; i < 2; i++) if (c[i]) c[i]->f = f(c[i]->f);
       f = F{1, 0};
     }
   }
   void pull() {
     assert(!f);
-    if (l) l->push();
-    if (r) r->push();
-    sz = size(l) + size(r) + 1;
-    s = sum(l) + sum(r) + v;
+    for (int i = 0; i < 2; i++) if (c[i]) c[i]->push();
+    sz = size(c[0]) + size(c[1]) + 1;
+    s = sum(c[0]) + sum(c[1]) + v;
   }
-  void rotate(T p, T g) {
-    int o = p->sz;
-    p->push(), push();
-    assert(this == p->l || this == p->r);
-    if (this == p->l) p->l = r, r = p;
-    else p->r = l, l = p;
-    if (g) {
-      if (p == g->l) g->l = this;
-      else g->r = this;
-    }
-    /* assert(!g || g->l == this || g->r == this); */
-    p->pull(), pull();
-    assert(sz == o);
-  }
-  T top() {
-    if (!st.size()) return nullptr;
-    T x = st.back(); st.pop_back();
-    return x;
-  }
-  bool step() {
-    T x = top(), p = top(), g = top(), a = top();
-    if (!p) return false;
-    assert(x == this && p);
-    /* cout << x << endl << p << endl; */
-    /* assert(this == p->l || this == p->r); */
-    if (!g) rotate(p, g);
-    else if ((this == p->l) == (p == g->l)) p->rotate(g, a), rotate(p, a);
-    else rotate(p, g), rotate(g, a);
-    if (a) st.push_back(a);
-    st.push_back(x);
-    assert(!a || a->l == x || a->r == x);
-    return true;
-  }
-  T splay(int x) { // splays and returns new root
-    /* cout << this << endl; */
-    st.push_back(this);
+  bool side() { return p->c[1] == this; }
+  void attach(int i, T x) {
+    if (x) x->p = this;
+    if (x) assert(x->p == this);
+    c[i] = x;
     push();
-    assert(x < sz);
-    if (x < size(l)) return l->splay(x);
-    else if (x > size(l)) return r->splay(x - size(l) - 1);
-    else { // we found the element
-      while (step());
-      /* cout << l->sz << " " << x << endl; */
-      /* assert(l->sz == x && p == ghost); */
-      return this;
+    pull();
+  }
+  T detach(int i) {
+    push();
+    T t = c[i];
+    if (c[i]) c[i]->p = nullptr;
+    c[i] = nullptr;
+    pull(); return t;
+  }
+  void rotate() {
+    T p = this->p, g = p->p;
+    p->push(), push();
+    int i = side();
+    if (g) g->attach(p->side(), this);
+    else this->p = nullptr;
+    assert(this == p->c[i]);
+    p->attach(i, c[!i]);
+    attach(!i, p);
+  }
+  void splay() {
+    for (;p;rotate()) {
+      if (p->p) {
+        (side() == p->side()) ? p->rotate() : rotate();
+      }
     }
+  }
+  T find(int i) {
+    if (c[0]) assert(c[0]->p == this);
+    if (c[1]) assert(c[1]->p == this);
+    assert(i <= sz);
+    push();
+    if (i < size(c[0])) return c[0]->find(i);
+    else if (i > size(c[0])) return c[1]->find(i - size(c[0]) - 1);
+    else return this;
   }
   friend ostream& operator<<(ostream& os, T s) {
     if (!s) return os;
-    os << s->l;
+    os << s->c[0];
     os << s->v << " ";
-    return os << s->r;
+    return os << s->c[1];
   }
 };
 
+Node Node::t[2 * N];
+
 struct Splay {
   T root = nullptr;
-  void splay(int x) { assert(!Node::st.size()); root = root->splay(x); }
-  auto split(int i) { // {[0, i), [i, )}
+  T splay(int x) {
+    T t = root->find(x);
+    t->splay();
+    assert(Node::size(t->c[0]) == x);
+    return root = t;
+  }
+  pair<Splay, Splay> split(int i) { // splits into {[0, i), [i, )}
     if (i == (root ? root->sz : 0)) return make_pair(Splay{root}, Splay{nullptr});
-    /* cout << root->sz << "y" << endl; */
-    /* cout << "splitting" << endl; */
-    splay(i);
-    assert(root != nullptr);
-    T l = root->l;
-    root->l = nullptr;
-    /* cout << root->sz << "x" << endl; */
-    /* cout << "post-split" << endl; */
-    root->pull();
-    return make_pair(Splay{l}, Splay{root});
+    T t = splay(i)->detach(0);
+    assert(root->c[0] == nullptr);
+    return make_pair(Splay{t}, Splay{root});
   }
   Splay join(Splay o) {
     if (!root) return root = o.root, *this;
     splay(root->sz - 1);
-    root->r = o.root;
-    /* cout << "post-join" << endl; */
-    root->pull();
+    assert(root->c[1] == nullptr);
+    root->attach(1, o.root);
     return *this;
   }
   void insert(int i, int x) {
     auto [l, r] = split(i);
-    root = new Node(x, l.root, r.root);
+    root = Node::init(x, l.root, r.root);
   }
   void del(int i) {
+    assert(i < root->sz);
     splay(i);
-    root = Splay{root->l}.join(Splay{root->r}).root;
+    root = Splay{root->detach(0)}.join(Splay{root->detach(1)}).root;
   }
   mint query(int l, int r) { // query range [l, r)
     Splay a, b, c;
@@ -175,18 +173,10 @@ signed main() {
   for (int i = 0; i < n; i++) {
     int x; cin >> x;
     splay.insert(i, x);
-    /* cout << splay << endl; */
   }
-  /* splay.insert(2, 1); */
-  /* cout << splay << endl; */
-  /* splay.insert(0, 10); */
-  /* cout << splay << endl; */
-  /* splay.del(2); */
-  /* cout << splay << endl; */
   while (q--) {
     int t; cin >> t;
     int i, x, l, r, w, b;
-    /* cout << splay << endl; */
     switch (t) {
       case 0:
         cin >> i >> x; splay.insert(i, x);
@@ -207,6 +197,5 @@ signed main() {
         cout << splay.query(l, r) << endl;
         break;
     }
-
   }
 }
