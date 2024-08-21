3bfa81d7f7089eec6202d4ffafc65eb87a8221e0
Daniel Zhu <danielfangzhu@gmail.com>
2024-08-20 23:25:57 -0700

    solve enchanted forest

--- loj/enchanted_forest.cc
+++ loj/enchanted_forest.cc
@@ -1,34 +1,36 @@
 #include <cassert>
 #include <functional>
+#include <algorithm>
 #include <iostream>
 #define int long long
 using namespace std;
 
-const int N = 1e5 + 1;
+const int N = 2e5 + 1;
 const int inf = 1e18;
 struct Node;
 typedef struct Node * T;
 struct Node {
   int v, id, sz, U, V;
-  T m; // node with max value in the subtree
+  T m = this;
   inline static int n = 0;
   static Node t[N];
   array<T, 2> c{};
   T p{};
   bool rev = false;
   Node() {}
-  Node(int id, int v) : v(v), m(this), id(id), sz(1), rev(false) {}
-  static T init(int id, int v, T l = nullptr, T r = nullptr) {
-    t[n] = Node(id, v);
+  Node(int id, int v, int U, int V) : v(v), id(id), sz(1), U(U), V(V) {}
+  static T init(int id, int v, int U = 0, int V = 0, T l = nullptr, T r = nullptr) {
+    t[n] = Node(id, v, U, V);
     t[n].attach(0, l);
     t[n].attach(1, r);
     return &t[n++];
   }
+  static int size(T x) { return x ? x->sz : 0; }
   static T mx(T x) { return x ? x->m : nullptr; }
   static T max(T a, T b) {
     if (!a) return b;
     if (!b) return a;
-    return a->v < b->v ? b : a;
+    return a->v > b->v ? a : b;
   }
   T push() {
     if (rev) {
@@ -40,6 +42,7 @@ struct Node {
   }
   void pull() {
     for (T x : c) if (x) x->push();
+    sz = size(c[0]) + size(c[1]) + 1;
     m = max(mx(c[0]), max(this, mx(c[1])));
   }
   bool side() { return p->c[1] == this; }
@@ -87,7 +90,7 @@ Node Node::t[N]{};
 
 struct LCT {
   T x[N]{}; // nodes
-  /* int r = 0; */
+  int n = 0;
   T access(int u, bool lca = false) {
     for (T y = x[u], z = nullptr; y; z = y, y = y->p) {
       y->splay();
@@ -97,11 +100,6 @@ struct LCT {
     x[u]->splay();
     return x[u];
   }
-  void print_chains() {
-    /* cout << "chains: " << endl; */
-    /* for (int i = 1; i <= 10; i++) if (x[i] && x[i]->is_root()) cout << x[i] << endl; */
-    /* cout << "----" << endl; */
-  }
   int root(int u) { // get root of forest containing u
     T x = access(u);
     while ((x->push())->c[0]) x = x->c[0];
@@ -109,10 +107,12 @@ struct LCT {
   }
   T reroot(int u) {
     access(u)->rev ^= true;
-    return x[u]->push();
+    return x[u];
   }
-  int node(int id, int v) {
-    if (!x[id]) x[id] = Node::init(id, v);
+  int node(int id, int v, int U = 0, int V = 0) {
+    assert(!x[id]);
+    x[id] = Node::init(id, v, U, V);
+    n = max(n, id + 1);
     return id;
   }
   void link(int u, int v) { // add v as child of u
@@ -121,20 +121,21 @@ struct LCT {
   int cut(int u) { // cut edge from u to parent
     return access(u)->detach(0), u;
   }
-  int query(int u, int v, function<int(T)> f) {
-    /* int R = root(u); */
+  template <typename U>
+  U query(int u, int v, function<U(T)> f) {
+    int R = root(u);
     reroot(u);
     assert(root(v) == u);
-    int x = f(access(v));
-    /* reroot(R); */
+    U x = f(access(v));
+    reroot(R);
     return x;
   }
   void upd(int u, int v, function<void(T)> f) {
-    /* int R = root(u); */
+    int R = root(u);
     reroot(u);
     assert(root(v) == u);
     f(access(v));
-    /* reroot(R); */
+    reroot(R);
   }
   int lca(int u, int v) {
     access(u);
@@ -142,44 +143,36 @@ struct LCT {
   }
 } lct;
 
-int a[N];
+void add_edge(int u, int v, int w) {
+  int x = lct.node(lct.n, w, u, v);
+  if (lct.root(u) == lct.root(v)) {
+    // cut maximum edge
+    T t = lct.query<T>(u, v, [&](T x) { return x->m; });
+    if (t->v <= w) return;
+    lct.reroot(t->id);
+    lct.cut(t->U);
+    lct.cut(t->V);
+  }
+  lct.link(x, u);
+  lct.link(x, v);
+}
+
+struct E {
+  int u, v, a, b;
+  bool operator<(const E o) const { return a < o.a; }
+} e[N];
 
 signed main() {
   cin.tie(0)->sync_with_stdio(0);
-  int n, q; cin >> n >> q;
-  for (int i = 1; i <= n; i++) {
-    cin >> a[i];
-    lct.node(i, a[i]);
+  int n, m; cin >> n >> m;
+  for (int i = 1; i <= n; i++) lct.node(i, 0);
+  for (int i = 0; i < m; i++) cin >> e[i].u >> e[i].v >> e[i].a >> e[i].b;
+  sort(e, e + m);
+  int r = inf;
+  for (int i = 0; i < m; i++) {
+    auto [u, v, a, b] =  e[i];
+    add_edge(u, v, b);
+    if (lct.root(1) == lct.root(n)) r = min(r, a + lct.query<int>(1, n, [&](T x) { return x->m->v; }));
   }
-  for (int i = 1; i <= n; i++) assert(lct.x[i]);
-  for (int i = 1; i < n; i++) {
-    int u, v; cin >> u >> v;
-    lct.link(u, v);
-  }
-  int r; cin >> r;
-  lct.reroot(r);
-  while (q--) {
-    int k; cin >> k;
-    if (k == 0) { int x; cin >> x; lct.reroot(x); }
-    else if (k < 3) {
-      int x, y, z; cin >> x >> y >> z;
-      cout << k << " " << x << " " << y << " " << z << endl;
-      cout << "stuff: " << (k == 2) << endl;
-      lct.upd(x, y, [=](T t) { t->f = F{k == 2, z}(t->f); });
-    }
-    else if (k < 6) {
-      int x, y; cin >> x >> y;
-      cout << k << " " << x << " "  << y << endl;
-      cout << lct.query(x, y, [=](T t) { return k == 3 ? t->m : (k == 4 ? t->M : t->s); }) << endl;
-    }
-    else if (k == 6) {
-      int x, y; cin >> x >> y;
-      if (lct.lca(x, y) == x) continue;
-      lct.link(y, lct.cut(x));
-    }
-    else {
-      int x, y; cin >> x >> y;
-      cout << lct.lca(x, y) << endl;
-    }
-  }
+  cout << (r == inf ? -1 : r) << endl;
 }
