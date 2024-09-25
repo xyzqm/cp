12556b425fd71118f5384e6394ecd0fa8745221e
Daniel Zhu <danielfangzhu@gmail.com>
2024-09-24 21:18:06 -0700

    update seg tree implementation

--- impls/seg_tree.cc
+++ impls/seg_tree.cc
@@ -1,42 +1,50 @@
+// https://judge.yosupo.jp/problem/point_add_range_sum
 #include <iostream>
-#define COMB(a, b) (a) + (b)
+#include <vector>
+#include <cassert>
 using namespace std;
 using ll = long long;
-const int N = 2e5 + 1;
-int n, q, a[N];
-ll seg[4 * N];
-ll query(int i, int j, int p = 1, int l = 1, int r = n) {
-    if (r < i || l > j) return 0;
-    if (i <= l && r <= j) return seg[p];
-    int m = (l + r) / 2;
-    return COMB(query(i, j, 2 * p, l, m), query(i, j, 2 * p + 1, m + 1, r));
-}
-void upd(int i, int x, int p = 1, int l = 1, int r = n) {
-    if (l == r) seg[p] = x;
-    else {
+const int N = 5e5 + 1;
+#define COMB(a, b) (a) + (b)
+int n;
+ll a[4 * N];
+struct SGT {
+  ll a[4 * N];
+  ll query(int i, int j, int p = 1, int l = 0, int r = n) {
+      if (r <= i || l >= j) return 0;
+      else if (i <= l && r <= j) return a[p];
+      else {
         int m = (l + r) / 2;
-        if (i <= m) upd(i, x, 2 * p, l, m);
-        else upd(i, x, 2 * p + 1, m + 1, r);
-        seg[p] = COMB(seg[2 * p], seg[2 * p + 1]);
+        return COMB(query(i, j, 2 * p, l, m), query(i, j, 2 * p + 1, m, r));
+      }
+  }
+  void upd(int i, int x, int p = 1, int l = 0, int r = n) {
+      if (r - l == 1) a[p] += x;
+      else {
+          int m = (l + r) / 2;
+          if (i < m) upd(i, x, 2 * p, l, m);
+          else upd(i, x, 2 * p + 1, m, r);
+          a[p] = COMB(a[2 * p], a[2 * p + 1]);
+      }
+  }
+  void build(ll* l, ll* r, int p = 1) {
+    if (r - l == 1) a[p] = *l;
+    else if (r - l > 1) {
+        ll* m = l + (r - l) / 2;
+        build(l, m, 2 * p);
+        build(m, r, 2 * p + 1);
+        a[p] = COMB(a[2 * p], a[2 * p + 1]);
     }
-}
-void build(int p = 1, int l = 1, int r = n) {
-    if (l == r) seg[p] = a[l];
-    else {
-        int m = (l + r) / 2;
-        build(2 * p, l, m); 
-        build(2 * p + 1, m + 1, r);
-        seg[p] = COMB(seg[2 * p], seg[2 * p + 1]);
-    }
-}
+  }
+} sgt;
 int main() {
-    cin >> n >> q;
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    build();
+    int q; cin >> n >> q;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sgt.build(a, a + n);
     while (q--) {
         int x, y, z;
         cin >> x >> y >> z;
-        if (x == 1) upd(y, z);
-        else cout << query(y, z) << endl;
+        if (!x) sgt.upd(y, z);
+        else cout << sgt.query(y, z) << endl;
     }
 }
