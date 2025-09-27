f13b69fce2e8269f27e4e100994c3bf64dfc3339
Daniel Zhu <danielfangzhu@gmail.com>
2025-09-27 14:52:15 -0700

    minor fix

--- ioi/llama.cc
+++ ioi/llama.cc
@@ -10,7 +10,6 @@ array<int, 3> nx[N][K]; // result + min and max indices to jump to segment
 vector<int> ft, st[K];
 
 int query(int l, int r) {
-    assert(l < r);
     int lg = __lg(r - l);
     return max(st[lg][l], st[lg][r - (1 << lg)]);
 }
@@ -48,10 +47,7 @@ void initialize(vector<int> T, vector<int> H) {
     for (int i = -1; i <= sz(H); i++) s.insert(i); // store all blocked indices
     for (auto it = hs.begin(); auto &[k, v] : ev_ft) {
         for (; it != hs.end() && it->first < k; it++) s.erase(it->second);
-        for (int x : v) {
-            assert(k == -1 || k > H[x]);
-            l[x] = *prev(s.upper_bound(x)), r[x] = *s.upper_bound(x);
-        }
+        for (int x : v) l[x] = *prev(s.upper_bound(x)), r[x] = *s.upper_bound(x);
     }
 
     s = {-1, sz(H)}; // now store available indices
@@ -62,8 +58,6 @@ void initialize(vector<int> T, vector<int> H) {
             nx[x][0] = {ft[a] < ft[b] ? a : b, a, b};
             if (nx[x][0][1] >= r[x]) nx[x][0] = {x, x, x};
             int y = nx[x][0][0];
-            assert(ft[y] >= ft[x]);
-            assert(l[y] <= l[x] && r[x] <= r[y]);
         }
     }
 
@@ -71,11 +65,9 @@ void initialize(vector<int> T, vector<int> H) {
         auto [a, b, c] = nx[i][k];
         auto [d, e, f] = nx[a][k];
         nx[i][k + 1] = {d, max(b, e), min(c, f)};
-        assert(ft[d] >= ft[i]);
     }
 }
 
-int t = 0;
 bool can_reach(int L, int R, int S, int D) {
     if (S > D) swap(S, D);
     int mx = query(S, D + 1);
