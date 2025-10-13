1df0a9cdf254dd549d6c745157a566964f383930
Daniel Zhu <danielfangzhu@gmail.com>
2025-10-12 21:39:57 -0700

    update template

--- template.cc
+++ template.cc
@@ -16,8 +16,8 @@ struct mint {
     int v = 0;
     mint() {}
     mint(int v) : v(v) {}
-    mint operator*(mint o) { return {v * o.v % M}; }
-    mint operator+(mint o) { return {(v + o.v) % M}; }
+    mint operator*(mint o) { return v * o.v % M; }
+    mint operator+(mint o) { return (v + o.v) % M; }
     void operator+=(mint o) { v = (v + o.v) % M; }
     mint operator-(mint o) { if (int x = v - o.v; x < 0) return x + M; else return x; }
     mint inv() {
