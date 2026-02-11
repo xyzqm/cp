662bd437e760fd54e498b8d029849e1821f4c115
Daniel Zhu <danielfangzhu@gmail.com>
2026-02-10 23:04:01 -0800

    reset run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 start=$(gdate +%s%3N)
 file=$1
 echo "compiling $file"
-clang++ -std=c++23 -O2 -Wall -I./lib -I./include $file -g || exit 1
-# clang++ -std=c++23 -O2 -Wall -fsanitize=undefined -fsanitize=address -I./lib -I./include $file -g || exit 1
+# clang++ -std=c++23 -O2 -Wall -I./lib -I./include $file -g || exit 1
+clang++ -std=c++23 -O2 -Wall -fsanitize=undefined -fsanitize=address -I./lib -I./include $file -g || exit 1
 echo "compiled $file in $(($(gdate +%s%3N) - start)) ms"
 start=$(gdate +%s%3N)
 MallocNanoZone=0 ./a.out<a.in || exit 1
