9674a3aaf4ea37ebef1fd7c70f8b92088cac1363
Daniel Zhu <danielfangzhu@gmail.com>
2026-03-28 19:11:35 -0700

    delete extraneous file

--- str.sh
+++
@@ -1,9 +0,0 @@
-g++ -std=c++23 -DEVAL $1 -o a
-g++ -std=c++23 -DEVAL $2 -o b
-for ((i = 1; ; i++)); do
-    echo $i
-    python3 $3 > test.in
-    ./a < test.in > a.out || break
-    ./b < test.in > b.out || break
-    diff -w a.out b.out || break
-done
